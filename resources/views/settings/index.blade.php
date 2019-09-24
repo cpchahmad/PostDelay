276e963e450d25e515193809fba4dea47d0767d9
Ahmad <cp.chahmad@gmail.com>
2019-09-25 00:25:56 +0500

    'aa'

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,4 +1,31 @@
 @extends('layout.admin')
 @section('content')
+    <div class="row" style="margin-top:-60px;">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <div class="row align-items-center">
+                    <div class="col-md-8">
+                        <h4 class="page-title m-0">Settings</h4>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="float-right d-none d-md-block">
+                            <div class="dropdown">
+                                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="ti-settings mr-1"></i> Settings
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Separated link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+            </div>
+        </div>
+    </div>
 @endsection
