b76feab01d8b9ddfa9b605406e2455abb3bbc5b6
Ahmad <cp.chahmad@gmail.com>
2019-09-24 20:11:44 +0500

    'aa'

---
+++ resources/views/email.blade.php
@@ -0,0 +1,19 @@
+<html>
+<title>
+    {{$subject}}
+</title>
+<body>
+<div style="text-align: center;font-size: 28px;font-weight: bold;">
+    Heading
+</div>
+<div style="text-align: center">
+    <p> {{$content}} </p>
+ </div>
+<div style="text-align: center;margin-top:30px;">
+    <a href="{{$image}}" target="_blank" style="    background-color: #eb1b90;
+    padding: 10px 20px 5px;
+    text-decoration: none;
+    color: white;">View Image</a>
+</div>
+</body>
+</html>
