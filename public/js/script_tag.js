cfc1601ade72dffb5ca14cdc8988de3b2b320fc7
fazal@novasolutions.dev <FazalGithub@1122>
2019-10-10 15:58:24 +0500

    aa

--- public/js/script_tag.js
+++ public/js/script_tag.js
@@ -42,7 +42,7 @@
                                   "                <p class='Custom-message-desp'>Your order has been received and payment was successful</p>\n" +
                                   "                <p class='Custom-message-desp'>The order ID is "+ shopifyorderid+"</p>\n" +
                                   "                <p class='Custom-message-desp'>In the order for us to link your package with this order,</p>\n" +
-                                  "                <p class='Custom-message-desp'>Please <p id='pdf_button' style='border: #0056b3 1px solid;cursor: pointer;color: #0056b3' data-order='"+ shopifyorderid +"' data-url='https://postdelay.shopifyapplications.com/download/pdf'>  Print out this form</p> and include it in your shipment to us </p>\n" +
+                                  "                <p class='Custom-message-desp'><span id='pdf_button' style='border-bottom: #0056b3 1px solid;cursor: pointer;color: #0056b3' data-order='"+ shopifyorderid +"' data-url='https://postdelay.shopifyapplications.com/download/pdf'>  Print out this form</span> and include it in your shipment to us </p>\n" +
                                   "                <p class='Custom-message-desp'>if you don't have a printer and would like the form mailed to you </p>\n" +
                                   "                <p class='Custom-message-desp'>Instead this service is available for $1.00</p>\n" +
                                   "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=request-form&&order-id="+shopifyorderid+"'>Click here</a> to request a paper & copy of the form </p>\n" +
