44d945d9b3c75de696bd9a80c04680a134804f69
Ahmad <cp.chahmad@gmail.com>
2019-10-22 19:13:31 +0500

    'aa'

--- resources/views/customers/existing_orders.blade.php
+++ resources/views/customers/existing_orders.blade.php
@@ -14,7 +14,10 @@
         <p class="ex-order-date">{{date_format($order->created_at,'Y-m-d')}}</p>
     </div>
     <div class="ex-order-coloum1 adj">
-        <p class="ex-order-id" style="max-width: fit-content">{{$order->has_status->name}}</p>
+        <div class="status_box">
+            <span style="background: {{ $order->has_status->color }};"></span>
+        <p class="ex-order-id" style="@if($order->has_status->color )color:{{ $order->has_status->color }}@else black @endif; max-width: fit-content">{{$order->has_status->name}}</p>
+        </div>
     </div>
     <div class="ex-order-coloum3">
         <input type="submit" onclick="window.location.href='account/orders/{{$order->token}}'" class="Same-button" value="View Order">
