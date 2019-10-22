d8412eb23fde6ba1921c0dd3cf617761f396299c
Ahmad <cp.chahmad@gmail.com>
2019-10-22 19:07:57 +0500

    'aa'

--- resources/views/customers/inc/order_status.blade.php
+++ resources/views/customers/inc/order_status.blade.php
@@ -1,5 +1,2 @@
 <h1 style="margin-bottom: 5px" class="Form-content-header-Head2">Order Status</h1>
-<p style="color: black;font-size: 14px">{{$order->has_status->name}}</p>
-{{--<select disabled name="Address-type-list" >--}}
-{{--    <option value="Address-Type-1">{{$order->has_status->name}}</option>--}}
-{{--</select>--}}
+<p style="@if($order->has_status->color )color:{{ $order->has_status->color }}@else black ;@endif font-size: 14px">{{$order->has_status->name}}</p>
