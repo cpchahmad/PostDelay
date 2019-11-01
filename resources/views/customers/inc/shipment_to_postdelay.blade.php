6deb86d7f8aecaba7a995b91e48727b935437d2e
fazal@novasolutions.dev <FazalGithub@1122>
2019-11-01 15:48:56 +0500

    aa

--- resources/views/customers/inc/shipment_to_postdelay.blade.php
+++ resources/views/customers/inc/shipment_to_postdelay.blade.php
@@ -2,20 +2,24 @@
     <div class="custom_fields_half">
         <div class="custom_Request_fields_half tow-field-Row-left ">
             <label for="Order ID">Ship Date</label>
-            <input type="date" required="" name="ship-date"  value="{{\Carbon\Carbon::parse($order->ship_to_postdelay_date)->format('Y-m-d')}}" placeholder="" min="{{now()->format('Y-m-d')}}" max="{{\Carbon\Carbon::parse($order->ship_out_date)->format('Y-m-d')}}">
+            <input type="date" required="" name="ship-date" @if($order->status_id == 2) disabled @endif  value="{{\Carbon\Carbon::parse($order->ship_to_postdelay_date)->format('Y-m-d')}}" placeholder="" min="{{now()->format('Y-m-d')}}" max="{{\Carbon\Carbon::parse($order->ship_out_date)->format('Y-m-d')}}">
         </div>
     </div>
     <div class="custom_fields_half">
         <div class="custom_Request_fields_half tow-field-Row-right">
             <label for="Order ID">Ship Method</label>
-            <input type="text" required="" name="ship-method" value="{{$order->ship_method}}" placeholder="">
+            <input type="text" required="" name="ship-method" @if($order->status_id == 2) disabled @endif value="{{$order->ship_method}}" placeholder="">
         </div>
     </div>
 </div>
 <div class="custom_fields_half">
     <div class="custom_Request_fields_half ">
         <label for="Order ID">Tracking Id</label>
-        <input type="text" required="" name="tracking_id"  value="{{$order->tracking_id}}" placeholder="">
+        <input type="text" required="" name="tracking_id" @if($order->status_id == 2) disabled @endif  value="{{$order->tracking_id}}" placeholder="">
     </div>
 </div>
+@if($order->status_id == 2)
+    <input type="button" class="Same-button" value="Save" disabled>
+    @else
 <input type="submit" class="Same-button" value="Save">
+    @endif
