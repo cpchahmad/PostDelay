6eccbec226c660400a7bcba0461755fd5fdd3886
Ahmad <cp.chahmad@gmail.com>
2019-11-21 20:10:49 +0500

    'aa'

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -239,7 +239,6 @@ class CustomersController extends Controller
 //                    ]
 //                ]);
 
-        dd($this->helper->getShop());
                     Customer::where('shopify_customer_id', $request->input('customer_id'))->update([
                         'first_name' => $request->input("first_name"),
                         'last_name' => $request->input("last_name"),
