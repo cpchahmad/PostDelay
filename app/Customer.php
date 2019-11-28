79d9e95208cdd79d904023000735d41b7db9c206
fazal@novasolutions.dev <FazalGithub@1122>
2019-11-28 19:41:19 +0500

    28112019F

--- app/Customer.php
+++ app/Customer.php
@@ -8,7 +8,7 @@ class Customer extends Model
 {
     protected $fillable = [
         'first_name','last_name', 'email', 'password','business','address1','address2','city','state','postcode'
-        ,'country','phone','email_verified_at','shopify_customer_id','shop_id'
+        ,'country','phone','email_verified_at','shopify_customer_id','shop_id','accept_marketing'
     ];
 
     public function has_Shop(){
