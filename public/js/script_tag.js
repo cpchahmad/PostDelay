041a2a0d42069ff4e79bfc30ff4764d18f515802
fazal@novasolutions.dev <FazalGithub@1122>
2019-12-11 13:07:41 +0500

    27112019F

--- public/js/script_tag.js
+++ public/js/script_tag.js
@@ -42,9 +42,9 @@
                                   "          <div class='Form-wraper'> \n" +
                                   "            <div class='Form-contaner'> \n" +
                                   "              <div class='Custom-message'>\n" +
-                                  "                <p class='Custom-message-desp'>Your order has been received and payment was successful</p>\n" +
-                                  "                <p class='Custom-message-desp'>The order ID is "+ shopifyorderid+"</p>\n" +
-                                  "                <p class='Custom-message-desp'>In the order for us to link your package with this order,</p>\n" +
+                                  "                <p class='Custom-message-desp'>Your order has been received and payment was successful </p><br>\n" +
+                                  // "                <p class='Custom-message-desp'>The order ID is "+ shopifyorderid+"</p>\n" +
+                                  // "                <p class='Custom-message-desp'>In the order for us to link your package with this order,</p>\n" +
                                   // "                <p class='Custom-message-desp'><span id='pdf_button' style='border-bottom: #0056b3 1px solid;cursor: pointer;color: #0056b3' data-order='"+ shopifyorderid +"' data-url='https://postdelay.shopifyapplications.com/download/pdf'>  Print out this form</span> and include it in your shipment to us </p>\n" +
 
                                   "                <p class='Custom-message-desp'><a href='https://postdelay.shopifyapplications.com/download/pdf?order="+ shopifyorderid +"'>  Print out this form</a> and include it in your shipment to us </p>\n" +
@@ -52,7 +52,7 @@
 
                                   "                <p class='Custom-message-desp'>if you don't have a printer and would like the form mailed to you </p>\n" +
                                   "                <p class='Custom-message-desp'>Instead this service is available for $1.00</p>\n" +
-                                  "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=request-form&&order-id="+shopifyorderid+"'>Click here</a> to request a paper & copy of the form </p>\n" +
+                                  "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=request-form&&order-id="+shopifyorderid+"'>Click here</a> to request a paper & copy of the form </p><br>\n" +
                                   "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=existing-orders'>Click here</a> to view the details of your order</p>\n" +
                                   "              </div>\n" +
                                   "            </div>\n" +
@@ -73,8 +73,8 @@
                                   "          <div class='Form-wraper'> \n" +
                                   "            <div class='Form-contaner'> \n" +
                                   "              <div class='Custom-message'>\n" +
-                                  "                <p class='Custom-message-desp'>Your payment has been received </p>\n" +
-                                  "                <p class='Custom-message-desp'>The order ID for this payment is "+ shopifyorderid+"</p>\n" +
+                                  "                <p class='Custom-message-desp'>Your payment has been received and your shipping label is on its way. </p>\n" +
+                                  // "                <p class='Custom-message-desp'>The order ID for this payment is "+ shopifyorderid+"</p>\n" +
                                   "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=existing-orders'>Click here</a> to view the details of your order</p>\n" +
                                   "              </div>\n" +
                                   "            </div>\n" +
