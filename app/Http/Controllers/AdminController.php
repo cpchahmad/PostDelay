275875fedec6134e150175924c233ff39ce9bb7a
Nova Solutions <fazal@novasolutions.dev>
2020-06-15 17:31:08 +0500

    15062020-download-pdf-fix!

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Customer;
 use App\Order;
 use App\OrderStatusHistory;
+use App\Settings;
 use App\Status;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -16,10 +17,19 @@ class AdminController extends Controller
         $order = Order::find($id);
         $logs = OrderStatusHistory::where('order_id',$id)->orderBy('change_at','desc')->get();
         $status = Status::all();
+        $settings = Settings::all()->first();
+        if($settings == null){
+            $settings =  new Settings();
+            $settings->min_threshold_ship_out_date = 7;
+            $settings->min_threshold_for_modify_ship_out_date = 5;
+            $settings->max_threshold_for_modify_ship_out_date = 5;
+            $settings->save();
+        }
         return view('orders.update_orders')->with([
             'logs' => $logs,
             'order' => $order,
-            'status' => $status
+            'status' => $status,
+            'settings' => $settings
         ]);
     }
     public function single_customer($id){
