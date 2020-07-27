9849edd7b73fc200eaeeb42d20a809d5a78b7ce6
Nova Solutions <fazal@novasolutions.dev>
2020-07-27 16:02:26 +0500

    tabs-remain-save

--- resources/views/customers/inc/shipment_to_postdelay.blade.php
+++ resources/views/customers/inc/shipment_to_postdelay.blade.php
@@ -2,21 +2,20 @@
     <div class="custom_fields_half">
         <div class="custom_Request_fields_half tow-field-Row-left ">
             <label for="Order ID">Ship Date</label>
-            <input type="date" required="" name="ship-date" @if($order->status_id == 2) disabled @endif @if($order->ship_to_postdelay_date != null)  value="{{\Carbon\Carbon::parse($order->ship_to_postdelay_date)->format('Y-m-d')}}" @else value="" @endif placeholder="" min="{{now()->format('Y-m-d')}}" max="{{\Carbon\Carbon::parse($order->ship_out_date)->format('Y-m-d')}}">
+            <input type="date" required="" name="ship-date"  @if($order->ship_to_postdelay_date != null)  value="{{\Carbon\Carbon::parse($order->ship_to_postdelay_date)->format('Y-m-d')}}" @else value="" @endif placeholder="" min="{{now()->format('Y-m-d')}}" max="{{\Carbon\Carbon::parse($order->ship_out_date)->format('Y-m-d')}}">
         </div>
     </div>
     <div class="custom_fields_half">
         <div class="custom_Request_fields_half tow-field-Row-right">
             <label for="Order ID">Ship Method</label>
-            <input type="text" required="" name="ship-method" @if($order->status_id == 2) disabled @endif value="{{$order->ship_method}}" placeholder="">
+            <input type="text" required="" name="ship-method" value="{{$order->ship_method}}" placeholder="">
         </div>
     </div>
 </div>
 <div class="custom_fields_half">
     <div class="custom_Request_fields_half ">
         <label for="Order ID">Tracking Id</label>
-        <input type="text" required="" name="tracking_id" @if($order->status_id == 2) disabled @endif  value="{{$order->tracking_id}}" placeholder="">
+        <input type="text" required="" name="tracking_id"  value="{{$order->tracking_id}}" placeholder="">
     </div>
 </div>
-
 <input type="submit" class="Same-button" value="Save">
