77cd96585d1b89f504371d3dc8e54fb13c811baa
Nova Solutions <fazal@novasolutions.dev>
2020-08-04 12:08:13 +0500

    script_tag fix 04082020F

--- public/js/script_tag.js
+++ public/js/script_tag.js
@@ -75,7 +75,7 @@
 
                           }
                       }
-                      else{
+                      else if(data.type === 0){
                           if ($('body .section__content').length > 0) {
                               if(response === '9'){
                                   $(".section__content .content-box").eq(0).after("<div class='content-box'>\n" +
@@ -145,7 +145,6 @@
                                       "  </div>\n" +
                                       "</div> ");
                               }
-
                               else if(response === '17'){
                                   $(".section__content .content-box").eq(0).after("<div class='content-box'>\n" +
                                       "  <div class='content-box__row text-container'>\n" +
@@ -180,8 +179,30 @@
                                       "  </div>\n" +
                                       "</div> ");
                               }
+                          }
+                      }
+                      else{
+                          if ($('body .section__content').length > 0) {
 
-
+                              $(".section__content .content-box").eq(0).after("<div class='content-box'>\n" +
+                                  "  <div class='content-box__row text-container'>\n" +
+                                  " <h2 class='heading-2 os-step__title' id='main-header' tabindex='-1' >PostDelay Message</h2>\n" +
+                                  "        <div class='page-width-1'> \n" +
+                                  "          <div class='Form-wraper'> \n" +
+                                  "            <div class='Form-contaner'> \n" +
+                                  "              <div class='Custom-message'>\n" +
+                                  "                <p class='Custom-message-desp'>Your order has been received and payment was successful </p><br>\n" +
+                                  "                <p class='Custom-message-desp'><a href='https://postdelay.shopifyapplications.com/download/pdf?order="+ shopifyorderid +"'>  Print out this form</a> and include it in your shipment to us </p>\n" +
+                                  "                <p class='Custom-message-desp'>if you don't have a printer and would like the form mailed to you </p>\n" +
+                                  "                <p class='Custom-message-desp'>Instead this service is available for $1.00</p>\n" +
+                                  "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=request-form&&order-id="+shopifyorderid+"'>Click here</a> to request a paper & copy of the form </p><br>\n" +
+                                  "                <p class='Custom-message-desp'><a href='https://postdelay.myshopify.com/account?view=existing-orders'>Click here</a> to view the details of your order</p>\n" +
+                                  "              </div>\n" +
+                                  "            </div>\n" +
+                                  "          </div>\n" +
+                                  "        </div>\n"+
+                                  "  </div>\n" +
+                                  "</div> ");
 
                           }
                       }
